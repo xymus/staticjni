da1d1334bb3eb7146c312538b5594c15641a1303
Alexis Laferrière <alexis.laf@xymus.net>
2012-11-27 23:38:00 -0500

    staticjni: adds a complex test for non-local meth., fields, const. and super
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

---
+++ test/tools/staticjni/complex/Complex.java
@@ -0,0 +1,65 @@
+package complex;
+
+import net.xymus.staticjni.NativeCall;
+import net.xymus.staticjni.NativeCalls;
+import net.xymus.staticjni.NativeNew;
+import net.xymus.staticjni.NativeSuperCall;
+
+public class Complex {
+	public static void main ( String[] args ) {
+		Complex a = new Complex();
+		a.Run();
+	}
+
+	public void Run() {
+		playWithMethods();
+		System.out.println( "hashCode: " + hashCode() );
+		System.out.println( "local_sub.i: " + local_sub.i );
+	}
+
+	public Sub local_sub;
+
+	/*
+	 * sets local_sub = sub
+	 * print local_sub.i
+	 * local_sub.i = something
+	 */
+	@NativeCalls( {"local_sub", "complex.Sub i", "playWithConstructors"} )
+	public native void playWithFields( Sub sub );
+
+	/*
+	 *  PlayWithStatics();
+	 *	Sub s = PlayWithConstructors();
+	 *	PlayWithFields(s);
+	 *  s.Foo();
+	 */
+	@NativeCalls( {"playWithStatics", "playWithFields", "complex.Sub foo"} )
+	public native void playWithMethods();
+
+	/*
+	 * Simply call StaticFoo
+	 */
+	@NativeCall( "StaticFoo" )
+	public native void playWithStatics();
+	static public void StaticFoo() {
+		System.out.println( "StaticFoo" );
+	}
+
+	/*
+	 * Instantiate a Sub and returns it
+	 */
+	@NativeNew( "complex.Sub" )
+	public native Sub playWithConstructors();
+
+	/*
+	 * returns super * 2
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	@NativeSuperCall
+	public native int hashCode();
+
+	static {
+		System.loadLibrary( "Complex" );
+	}
+}
---
+++ test/tools/staticjni/complex/Sub.java
@@ -0,0 +1,12 @@
+package complex;
+
+public class Sub {
+	public int i = 123;
+	
+	public Sub() {
+	}
+	
+	void foo() {
+		System.out.println("bar");
+	}
+}
